f9e033bb443b62f7e798ba9f6882cddb6e9de5b8
Guilherme de Nez Silvano <guisilvano@hotmail.com>
2019-06-19 09:18:21 -0300

    Update TableReplicationDirectionDAO.java

--- src/database/TableReplicationDirectionDAO.java
+++ src/database/TableReplicationDirectionDAO.java
@@ -14,6 +14,13 @@ public class TableReplicationDirectionDAO extends MasterDAO {
     
     private String is_selectOrigem = "select database_origem from tb_replicacao_direcao where nome = ?";
     
+   private String is_insertDirecao = "insert into tb_replicacao_direcao "
+    		+ "(codigo_direcao, data_atual, processo, database_origem, database_destino,"
+    		+ "usuario_origem, usuario_destino, senha_origem, senha_destino,"
+    		+ "automatico_manual, periodo_ano, periodo_mes, periodo_dia,"
+    		+ "periodo_hora, periodo_minuto, periodo_segundo, retencao, habilitado)"
+    		+ "values (default, current_date, ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+	
     private String is_selectDestino = "select database_destino from tb_replicacao_direcao where nome = ?";
     
     private String is_selectDirectionNames = "select codigo_direcao from tb_replicacao_direcao where processo = ?";
@@ -33,6 +40,7 @@ public class TableReplicationDirectionDAO extends MasterDAO {
     private PreparedStatement pst_select;
     private PreparedStatement pst_selectDirectionNames;
     private PreparedStatement pst_insert;
+	private PreparedStatement pst_insertDirecao;
     private PreparedStatement pst_delete;
     private PreparedStatement pst_selectOrigem;
     private PreparedStatement pst_selectDestino;
@@ -45,6 +53,7 @@ public class TableReplicationDirectionDAO extends MasterDAO {
         io_connection = connection;
         pst_select = connection.prepareStatement(is_select);
         pst_insert = connection.prepareStatement(is_insert);
+		pst_insertDirecao = connection.prepareStatement(is_insertDirecao);
         pst_selectDirectionNames = connection.prepareStatement(is_selectDirectionNames);
         pst_selectOrigem = connection.prepareStatement(is_selectOrigem);
         pst_selectDestino = connection.prepareStatement(is_selectDestino);
@@ -76,6 +85,33 @@ public class TableReplicationDirectionDAO extends MasterDAO {
 		return Direction;
     }
 
+	public void InsertDirecao(Object parameter) throws SQLException {
+
+		pst_insertDirecao.clearParameters();
+		
+		ReplicationDirection lo_replication = (ReplicationDirection)parameter;
+		
+		
+		Set(pst_insertDirecao, 1, lo_replication.getProcesso());
+		Set(pst_insertDirecao, 2, lo_replication.getDatabase_origem());
+		Set(pst_insertDirecao, 3, lo_replication.getDatabase_destino());
+		Set(pst_insertDirecao, 4, lo_replication.getUsuario_origem());
+		Set(pst_insertDirecao, 5, lo_replication.getUsuario_destino());
+		Set(pst_insertDirecao, 6, lo_replication.getSenha_origem());
+		Set(pst_insertDirecao, 7, lo_replication.getSenha_destino());
+		Set(pst_insertDirecao, 8, lo_replication.getAuto_manual());
+		Set(pst_insertDirecao, 9, lo_replication.getAno());
+		Set(pst_insertDirecao, 10, lo_replication.getMes());
+		Set(pst_insertDirecao, 11, lo_replication.getDia());
+		Set(pst_insertDirecao, 12, lo_replication.getHora());
+		Set(pst_insertDirecao, 13, lo_replication.getMinuto());
+		Set(pst_insertDirecao, 14, lo_replication.getSegundo());
+		Set(pst_insertDirecao, 15, lo_replication.getRetencao());
+		Set(pst_insertDirecao, 16, lo_replication.getHabilitado());
+		
+		pst_insertDirecao.execute();
+    }
+	
 	public String[] selectDirectionNames(String processo) throws SQLException {
 		
 		pst_selectDirectionNames.setString(1, processo);
